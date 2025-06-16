f8f71104ae415a7ef7874ddfb45c45dd9051be87
unknown <sutera@student.ethz.ch>
2025-06-16 11:41:25 +0200

    added image creation on click

--- js/app.js
+++ js/app.js
@@ -1,19 +1,25 @@
-let buttonHitCount=0;
+let buttonHitCount = 0;
 const hitmeButton = document.getElementById("button1");
 
-
-(()=> {
-    console.log("IIFE")
-
+(() => {
+  console.log("IIFE");
 })();
 
+// Moin Alex: Das isch en Basic EventListener
+// er luegts element "hitmeButton" ah & lost uf e spezifischi AKtion
+// idem Fall "Click"
+hitmeButton.addEventListener("click", () => {
+  //tun Variable um eis erhöche
+  buttonHitCount++;
+  //suech im Index.html nach em Element mit ID buttonHitCount, bearbeite Attribut innerHTML (Das passt denn de Inhalt devo ah)
+  document.getElementById("buttonHitCount").innerHTML =
+    "Button Hit Count: " + buttonHitCount;
+  // tuet es mooscles bild hinzufüege
+  const img = document.createElement("img");
+  img.src = "/mooscles.png";
+  img.width = 100;
+  img.style.margin = "5px";
 
-  // Moin Alex: Das isch en Basic EventListener
-  // er luegts element "hitmeButton" ah & lost uf e spezifischi AKtion 
-  // idem Fall "Click" 
-hitmeButton.addEventListener("click",()=>{
-    //tun Variable um eis erhöche
-    buttonHitCount++;
-    //suech im Index.html nach em Element mit ID buttonHitCount, bearbeite Attribut innerHTML (Das passt denn de Inhalt devo ah)
-    document.getElementById("buttonHitCount").innerHTML = "Button Hit Count: "+buttonHitCount
-})
+  // Add it to the container
+  document.getElementById("imageContainer").appendChild(img);
+});
